ef5cf0aec3a757ae3e641b0597477a6ac8eb0ab0
Sujit Walmiki <sujitw@caminosoftai.com>
2025-06-14 20:01:04 +0530

    Added DTOS for PlatFrom Create and Read

---
+++ PlatformService/DTOS/PlatformReadDto.cs
@@ -0,0 +1,18 @@
+
+namespace PlatformService.DTOS
+{
+    public class PlatformReadDto
+    {
+       
+      public int ID { get; set; }
+
+      
+      public string Name { get; set; }
+
+   
+      public string Publisher { get; set; }
+
+     
+      public string Cost { get; set; }
+    }
+}
